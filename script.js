a37232c6b229d0fba42f40d4979df22ae3fbc75e
EDUARDO BORGES CAMBRAIA <eduardo_cambraia@estudante.sesisenai.org.br>
2023-08-23 08:28:53 -0300

    sla

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+listaAnotacoes = []
+listaNumero = []
+listaData = []
+listaImagem = []
 
+function create(){
+    let texto = document.getElementById("pess").value 
+}
+function update(){
+
+}
+function del(){
+     
+}
