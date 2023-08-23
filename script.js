fe26df24e89a55f726c188b39519276fad05256d
EDUARDO BORGES CAMBRAIA <eduardo_cambraia@estudante.sesisenai.org.br>
2023-08-23 08:57:01 -0300

    kdksa

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ listaData = []
 listaImagem = []
 
 function create(){
-    let texto = document.getElementById("pess").value 
+    let texto = document.getElementById("anotacoes").value 
+    let img = document.getElementById("imagem").value 
+    let dat = document.getElementById("dia").value 
+    let num = document.getElementById("numero").value 
+
+    localStorage.setItem(listaAnotacoes, texto);
+    listaNumero.push(num);
+    listaImagem.push(img);
+    listaData.push(dat)
+    document.getElementById("anot").innerHTML = texto
 }
 function update(){
 
