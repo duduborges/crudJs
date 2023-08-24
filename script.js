3770f91b6745e12fddba2753f9efeaa496782b85
ARTHUR D'EÇA MORGENSTERN VINHAS MARCHETTI <arthur_d_marchetti@estudante.sesisenai.org.br>
2023-08-24 12:54:00 -0300

    Atualizado

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-listaAnotacoes = []
-listaNumero = []
-listaData = []
-listaImagem = []
+// Base de dados Nossa
+const miniRedeSocial = {
+    usuarios : [ 
+        {
+            username: `omario`,
+        }
+    ],
+    posts: [
+        {
+            id: 1,
+            owner: `omario`,
+            content: 'Crie Posts'
+        }
+    ],
+  criaPost(dados, htmlOnly = false){
+    if(!htmlOnly){
+    // Cria Posts na memória (Array/Objeto)
+    miniRedeSocial.posts.push({
+            id: miniRedeSocial.posts.length + 1,
+            owner: dados.owner,
+            content: dados.content
+           
+        });
+    }
+     // Cria post no HTML
+        const $listaDePosts = document.querySelector(".listaDePosts");
+        $listaDePosts.insertAdjacentHTML('afterbegin', `<li>${dados.content}</li>`);
+    }
+};
 
-function create(){
-    let texto = document.getElementById("anotacoes").value 
-    let img = document.getElementById("imagem").value 
-    let dat = document.getElementById("dia").value 
-    let num = document.getElementById("numero").value 
 
-    localStorage.setItem(listaAnotacoes, texto);
-    listaNumero.push(num);
-    listaImagem.push(img);
-    listaData.push(dat)
-    document.getElementById("anot").innerHTML = texto
-}
-function update(){
 
-}
-function del(){
-     
-}
+
+// [CODIGO de Front End: Web]
+document.addEventListener('DOMContentLoaded', function() {
+    const $meuForm = document.querySelector('form');
+
+//CRUD : [READ]
+miniRedeSocial.posts.forEach(( {owner, content}) => {
+    miniRedeSocial.criaPost({owner: owner, content: content}, true)
+})
+
+//CRUD : CREATE
+    $meuForm.addEventListener('submit', function criaPostController(infosDoEvento) {
+      infosDoEvento.preventDefault();
+      console.log("Novo Post enviado!")
+      const $campoCriaPost = document.querySelector('input[name="anotacoes"]');
+
+      miniRedeSocial.criaPost({ owner: `omario`, content: $campoCriaPost.value})
+      console.log(miniRedeSocial.posts);
+      // Limpa o campo de anotações após adicionar o post
+      $campoCriaPost.value = '';
+      
+    });
+  });
